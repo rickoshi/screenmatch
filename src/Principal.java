f029317ac2c713484f479b6495830b30e3568949
rickoshi <145235121+rickoshi@users.noreply.github.com>
2025-06-07 01:59:03 -0300

    Add features from aula03

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -12,8 +14,32 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("Média de avaliações: " + meuFilme.retornaMedia());
+        System.out.println("Duração: " + meuFilme.getDuracaoEmMinutos());
+        System.out.println();
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2004);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(6);
+        lost.setEpisodiosPorTemporada(20);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar: " + lost.getDuracaoEmMinutos());
+        System.out.println();
+
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("The Matrix");
+        outroFilme.setAnoDeLancamento(1999);
+        outroFilme.setDuracaoEmMinutos(136);
+        outroFilme.setIncluidoNoPlano(true);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
     }
 }
